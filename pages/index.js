aa716b577aa13c1509fcf6f2caa326df90a9d78a
Isaquemdn <69495705+Isaquemdn@users.noreply.github.com>
2025-08-30 23:04:56 +0000

    adicionando novamente o gitignore, pois eu havia colocado o node_modules incorretamente como .node_modules, fazendo com que permanecesse no untracked files. Também adiciono agora o diretório pages.

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>"Porque Deus amou o mundo de tal maneira, que deu o seu filho unigênito, para que todo aquele que nele crer não pereça, mas tenha a vida eterna"</h1>
+}
+
+export default Home;
